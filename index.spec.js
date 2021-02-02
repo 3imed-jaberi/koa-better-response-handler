49985153a7f38ca156d37e4c46a8b4780a08ab1e
3imed-jaberi <imed_jebari@hotmail.fr>
2021-02-02 15:04:24 +0100

    update test code --cov-100% 🧪 ..

--- index.spec.js
+++ index.spec.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
 const request = require('supertest')
 const should = require('should')
+const koaViews = require('koa-views')
+const koaQueryString = require('koa-qs')
 
 const responseHandler = require('.')
 
@@ -137,5 +139,180 @@ describe('koa-response-handler', () => {
     request(app.listen())
       .get('/')
       .expect(500, done)
+  })
+
+  it('koa-views: should find render function', (done) => {
+    const app = new Koa()
+      .use(koaViews())
+      .use(responseHandler())
+      .use(ctx => {
+        should(ctx.render).be.ok()
+        should(ctx.render).be.a.Function()
+        should(ctx.response.render).be.equal(ctx.render)
+        // should use `koa-views`.
+        should(!ctx.render.toString().includes('defaultNotImplMethods')).be.equal(true)
+      })
+
+    request(app.listen()).get('/').end(done)
+  })
+
+  it('koa-views: should throw when do not finde the render function', (done) => {
+    const app = new Koa()
+      .use(responseHandler())
+      .use(ctx => { should.throws(() => { ctx.render() }) })
+
+    request(app.listen()).get('/').end(done)
+  })
+
+  // test code from:
+  // https://github.com/koajs/koa-safe-jsonp/blob/master/test/index.test.js
+  // with some modification
+  describe('support jsonp', function () {
+    it('should send normal response when callback missing', function (done) {
+      const app = new Koa()
+        .use(responseHandler())
+        .use(function (ctx) {
+          ctx.jsonp({ foo: 'bar' })
+        })
+
+      request(app.listen())
+        .get('/foo.json?foo=fn')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect('{"foo":"bar"}')
+        .expect(200, done)
+    })
+
+    it('should send normal response when callback is empty string', function (done) {
+      const app = new Koa()
+        .use(responseHandler())
+        .use(function (ctx) {
+          ctx.jsonp({ foo: 'bar' })
+        })
+
+      request(app.listen())
+        .get('/foo.json?foo=fn&callback=')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect('{"foo":"bar"}')
+        .expect(200, done)
+    })
+
+    it('should send jsonp response with default callback', function (done) {
+      const app = new Koa()
+        .use(responseHandler())
+        .use(function (ctx) {
+          ctx.jsonp({ foo: 'bar' })
+        })
+
+      request(app.listen())
+        .get('/foo.json?callback=fn')
+        .expect('Content-Type', 'application/javascript; charset=utf-8')
+        .expect('X-Content-Type-Options', 'nosniff')
+        .expect('/**/ typeof fn === \'function\' && fn({"foo":"bar"});')
+        .expect(200, done)
+    })
+
+    it('should send jsonp response with array callback', function (done) {
+      const app = new Koa()
+      koaQueryString(app)
+      app
+        .use(responseHandler())
+        .use(function (ctx) {
+          ctx.jsonp({ foo: 'bar' })
+        })
+
+      request(app.listen())
+        .get('/foo.json?callback=fn&callback=cb')
+        .expect('Content-Type', 'application/javascript; charset=utf-8')
+        .expect('X-Content-Type-Options', 'nosniff')
+        .expect('/**/ typeof fn === \'function\' && fn({"foo":"bar"});')
+        .expect(200, done)
+    })
+
+    it('should send number response', function (done) {
+      const app = new Koa()
+        .use(responseHandler())
+        .use(function (ctx) {
+          ctx.jsonp(1984)
+        })
+
+      request(app.listen())
+        .get('/foo.json?callback=fn')
+        .expect('Content-Type', 'application/javascript; charset=utf-8')
+        .expect('X-Content-Type-Options', 'nosniff')
+        .expect("/**/ typeof fn === 'function' && fn(1984);")
+        .expect(200, done)
+    })
+
+    it('should send string response', function (done) {
+      const app = new Koa()
+        .use(responseHandler())
+        .use(function (ctx) {
+          ctx.jsonp('1984')
+        })
+
+      request(app.listen())
+        .get('/foo.json?callback=fn')
+        .expect('Content-Type', 'application/javascript; charset=utf-8')
+        .expect('X-Content-Type-Options', 'nosniff')
+        .expect('/**/ typeof fn === \'function\' && fn("1984");')
+        .expect(200, done)
+    })
+
+    it('should send jsonp response with custom callback', function (done) {
+      const app = new Koa()
+        .use(responseHandler({
+          jsonp: { callback: '_callback' }
+        }))
+        .use(function (ctx) {
+          ctx.jsonp({ foo: 'bar' })
+        })
+
+      request(app.listen())
+        .get('/foo.json?_callback=$jsonp_callback')
+        .expect('Content-Type', 'application/javascript; charset=utf-8')
+        .expect('X-Content-Type-Options', 'nosniff')
+        .expect(
+          '/**/ typeof $jsonp_callback === \'function\' && $jsonp_callback({"foo":"bar"});'
+        )
+        .expect(200, done)
+    })
+
+    it('should limit callback name length to 10', function (done) {
+      const app = new Koa()
+        .use(responseHandler({
+          jsonp: { callback: '_callback', limit: 10 }
+        }))
+        .use(function (ctx) {
+          ctx.jsonp({ foo: 'bar' })
+        })
+
+      request(app.listen())
+        .get(
+          '/foo.json?_callback=$123456789jsonp_callbackjsonp_callbackjsonp_callback'
+        )
+        .expect('Content-Type', 'application/javascript; charset=utf-8')
+        .expect('X-Content-Type-Options', 'nosniff')
+        .expect(
+          '/**/ typeof $123456789 === \'function\' && $123456789({"foo":"bar"});'
+        )
+        .expect(200, done)
+    })
+
+    it('should dont limit when callback name length < limit options', function (done) {
+      const app = new Koa()
+        .use(responseHandler({
+          jsonp: { callback: '_callback', limit: 10 }
+        }))
+        .use(function (ctx) {
+          ctx.jsonp({ foo: 'bar' })
+        })
+
+      request(app.listen())
+        .get('/foo.json?_callback=$123')
+        .expect('Content-Type', 'application/javascript; charset=utf-8')
+        .expect('X-Content-Type-Options', 'nosniff')
+        .expect('/**/ typeof $123 === \'function\' && $123({"foo":"bar"});')
+        .expect(200, done)
+    })
   })
 })
