de190f205a649d6435aa16915033e139d38417ee
3imed-jaberi <imed_jebari@hotmail.fr>
2021-12-08 00:14:57 +0100

    update test code --cov-100% 🧪 ..

--- index.spec.js
+++ index.spec.js
@@ -1,13 +1,13 @@
 const Koa = require('koa')
 const request = require('supertest')
 const should = require('should')
+const koaJsonp = require('koa-safe-jsonp')
 const koaViews = require('koa-views')
-const koaQueryString = require('koa-qs')
 
 const responseHandler = require('.')
 
 describe('koa-response-handler', () => {
-  it('have a statusCode method', done => {
+  it('have a statusCode method', async () => {
     const app = new Koa()
       .use(responseHandler())
       .use(ctx => {
@@ -16,12 +16,12 @@ describe('koa-response-handler', () => {
         should(ctx.response.statusCode).be.equal(ctx.statusCode)
       })
 
-    request(app.listen())
+    await request(app.listen())
       .get('/')
-      .expect(404, done)
+      .expect(404)
   })
 
-  it('have a sendStatus method', done => {
+  it('have a sendStatus method', async () => {
     const app = new Koa()
       .use(responseHandler())
       .use(ctx => {
@@ -30,12 +30,12 @@ describe('koa-response-handler', () => {
         should(ctx.response.sendStatus).be.equal(ctx.sendStatus)
       })
 
-    request(app.listen())
+    await request(app.listen())
       .get('/')
-      .expect(404, done)
+      .expect(404)
   })
 
-  it('have a send method', done => {
+  it('have a send method', async () => {
     const app = new Koa()
       .use(responseHandler())
       .use(ctx => {
@@ -44,12 +44,12 @@ describe('koa-response-handler', () => {
         should(ctx.response.send).be.equal(ctx.send)
       })
 
-    request(app.listen())
+    await request(app.listen())
       .get('/')
-      .expect(404, done)
+      .expect(404)
   })
 
-  it('have a json method', done => {
+  it('have a json method', async () => {
     const app = new Koa()
       .use(responseHandler())
       .use(ctx => {
@@ -58,12 +58,12 @@ describe('koa-response-handler', () => {
         should(ctx.response.json).be.equal(ctx.json)
       })
 
-    request(app.listen())
+    await request(app.listen())
       .get('/')
-      .expect(404, done)
+      .expect(404)
   })
 
-  it('should sendStatus method work', done => {
+  it('should sendStatus method work', async () => {
     const app = new Koa()
       .use(responseHandler())
       .use(ctx => {
@@ -72,14 +72,14 @@ describe('koa-response-handler', () => {
           .sendStatus()
       })
 
-    request(app.listen())
+    await request(app.listen())
       .get('/')
       .expect('Content-Type', /text/)
       .expect(/OK/)
-      .expect(200, done)
+      .expect(200)
   })
 
-  it('should send method work', done => {
+  it('should send method work', async () => {
     const app = new Koa()
       .use(responseHandler())
       .use(ctx => {
@@ -88,14 +88,14 @@ describe('koa-response-handler', () => {
           .send('hello world')
       })
 
-    request(app.listen())
+    await request(app.listen())
       .get('/')
       .expect('Content-Type', /text/)
       .expect(/hello world/)
-      .expect(200, done)
+      .expect(200)
   })
 
-  it('should json method work', done => {
+  it('should json method work', async () => {
     const app = new Koa()
       .use(responseHandler())
       .use(ctx => {
@@ -104,14 +104,14 @@ describe('koa-response-handler', () => {
           .json({ msg: 'hello world' })
       })
 
-    request(app.listen())
+    await request(app.listen())
       .get('/')
       .expect('Content-Type', /json/)
       .expect(/hello world/)
-      .expect(200, done)
+      .expect(200)
   })
 
-  it('should throw when pass bad option --is-json', done => {
+  it('should throw when pass bad option --is-json', async () => {
     const app = new Koa()
       .use(responseHandler({
         isJSON: 'this is string not function'
@@ -122,12 +122,12 @@ describe('koa-response-handler', () => {
           .json({ msg: 'hello world' })
       })
 
-    request(app.listen())
+    await request(app.listen())
       .get('/')
-      .expect(500, done)
+      .expect(500)
   })
 
-  it('should throw when pass bad json --is-json', done => {
+  it('should throw when pass bad json --is-json', async () => {
     const app = new Koa()
       .use(responseHandler())
       .use(ctx => {
@@ -136,12 +136,36 @@ describe('koa-response-handler', () => {
           .json('{ x: 10, , y: 10 }')
       })
 
-    request(app.listen())
+    await request(app.listen())
       .get('/')
-      .expect(500, done)
+      .expect(500)
   })
 
-  it('koa-views: should find render function', (done) => {
+  it('koa-safe-jsonp: should find jsonp function', async () => {
+    const app = new Koa()
+    koaJsonp(app)
+
+    app
+      .use(responseHandler())
+      .use(ctx => {
+        should(ctx.jsonp).be.ok()
+        should(ctx.jsonp).be.a.Function()
+        should(ctx.response.jsonp).be.equal(ctx.jsonp)
+        should(!ctx.jsonp.toString().includes('defaultNotImplMethods')).be.equal(true)
+      })
+
+    await request(app.listen()).get('/')
+  })
+
+  it('koa-safe-jsonp: should throw when do not find the jsonp function', async () => {
+    const app = new Koa()
+      .use(responseHandler())
+      .use(ctx => { should.throws(() => { ctx.render() }) })
+
+    await request(app.listen()).get('/')
+  })
+
+  it('koa-views: should find render function', async () => {
     const app = new Koa()
       .use(koaViews())
       .use(responseHandler())
@@ -153,166 +177,14 @@ describe('koa-response-handler', () => {
         should(!ctx.render.toString().includes('defaultNotImplMethods')).be.equal(true)
       })
 
-    request(app.listen()).get('/').end(done)
+    await request(app.listen()).get('/')
   })
 
-  it('koa-views: should throw when do not finde the render function', (done) => {
+  it('koa-views: should throw when do not find the render function', async () => {
     const app = new Koa()
       .use(responseHandler())
       .use(ctx => { should.throws(() => { ctx.render() }) })
 
-    request(app.listen()).get('/').end(done)
-  })
-
-  // test code from:
-  // https://github.com/koajs/koa-safe-jsonp/blob/master/test/index.test.js
-  // with some modification
-  describe('support jsonp', function () {
-    it('should send normal response when callback missing', function (done) {
-      const app = new Koa()
-        .use(responseHandler())
-        .use(function (ctx) {
-          ctx.jsonp({ foo: 'bar' })
-        })
-
-      request(app.listen())
-        .get('/foo.json?foo=fn')
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect('{"foo":"bar"}')
-        .expect(200, done)
-    })
-
-    it('should send normal response when callback is empty string', function (done) {
-      const app = new Koa()
-        .use(responseHandler())
-        .use(function (ctx) {
-          ctx.jsonp({ foo: 'bar' })
-        })
-
-      request(app.listen())
-        .get('/foo.json?foo=fn&callback=')
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect('{"foo":"bar"}')
-        .expect(200, done)
-    })
-
-    it('should send jsonp response with default callback', function (done) {
-      const app = new Koa()
-        .use(responseHandler())
-        .use(function (ctx) {
-          ctx.jsonp({ foo: 'bar' })
-        })
-
-      request(app.listen())
-        .get('/foo.json?callback=fn')
-        .expect('Content-Type', 'application/javascript; charset=utf-8')
-        .expect('X-Content-Type-Options', 'nosniff')
-        .expect('/**/ typeof fn === \'function\' && fn({"foo":"bar"});')
-        .expect(200, done)
-    })
-
-    it('should send jsonp response with array callback', function (done) {
-      const app = new Koa()
-      koaQueryString(app)
-      app
-        .use(responseHandler())
-        .use(function (ctx) {
-          ctx.jsonp({ foo: 'bar' })
-        })
-
-      request(app.listen())
-        .get('/foo.json?callback=fn&callback=cb')
-        .expect('Content-Type', 'application/javascript; charset=utf-8')
-        .expect('X-Content-Type-Options', 'nosniff')
-        .expect('/**/ typeof fn === \'function\' && fn({"foo":"bar"});')
-        .expect(200, done)
-    })
-
-    it('should send number response', function (done) {
-      const app = new Koa()
-        .use(responseHandler())
-        .use(function (ctx) {
-          ctx.jsonp(1984)
-        })
-
-      request(app.listen())
-        .get('/foo.json?callback=fn')
-        .expect('Content-Type', 'application/javascript; charset=utf-8')
-        .expect('X-Content-Type-Options', 'nosniff')
-        .expect("/**/ typeof fn === 'function' && fn(1984);")
-        .expect(200, done)
-    })
-
-    it('should send string response', function (done) {
-      const app = new Koa()
-        .use(responseHandler())
-        .use(function (ctx) {
-          ctx.jsonp('1984')
-        })
-
-      request(app.listen())
-        .get('/foo.json?callback=fn')
-        .expect('Content-Type', 'application/javascript; charset=utf-8')
-        .expect('X-Content-Type-Options', 'nosniff')
-        .expect('/**/ typeof fn === \'function\' && fn("1984");')
-        .expect(200, done)
-    })
-
-    it('should send jsonp response with custom callback', function (done) {
-      const app = new Koa()
-        .use(responseHandler({
-          jsonp: { callback: '_callback' }
-        }))
-        .use(function (ctx) {
-          ctx.jsonp({ foo: 'bar' })
-        })
-
-      request(app.listen())
-        .get('/foo.json?_callback=$jsonp_callback')
-        .expect('Content-Type', 'application/javascript; charset=utf-8')
-        .expect('X-Content-Type-Options', 'nosniff')
-        .expect(
-          '/**/ typeof $jsonp_callback === \'function\' && $jsonp_callback({"foo":"bar"});'
-        )
-        .expect(200, done)
-    })
-
-    it('should limit callback name length to 10', function (done) {
-      const app = new Koa()
-        .use(responseHandler({
-          jsonp: { callback: '_callback', limit: 10 }
-        }))
-        .use(function (ctx) {
-          ctx.jsonp({ foo: 'bar' })
-        })
-
-      request(app.listen())
-        .get(
-          '/foo.json?_callback=$123456789jsonp_callbackjsonp_callbackjsonp_callback'
-        )
-        .expect('Content-Type', 'application/javascript; charset=utf-8')
-        .expect('X-Content-Type-Options', 'nosniff')
-        .expect(
-          '/**/ typeof $123456789 === \'function\' && $123456789({"foo":"bar"});'
-        )
-        .expect(200, done)
-    })
-
-    it('should dont limit when callback name length < limit options', function (done) {
-      const app = new Koa()
-        .use(responseHandler({
-          jsonp: { callback: '_callback', limit: 10 }
-        }))
-        .use(function (ctx) {
-          ctx.jsonp({ foo: 'bar' })
-        })
-
-      request(app.listen())
-        .get('/foo.json?_callback=$123')
-        .expect('Content-Type', 'application/javascript; charset=utf-8')
-        .expect('X-Content-Type-Options', 'nosniff')
-        .expect('/**/ typeof $123 === \'function\' && $123({"foo":"bar"});')
-        .expect(200, done)
-    })
+    await request(app.listen()).get('/')
   })
 })
